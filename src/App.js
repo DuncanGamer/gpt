92ed8271071f63b5d3e715bcfbafe536ef758bcf
Daniel G <dgimenez123@gmail.com>
2023-07-10 14:26:24 +0200

    header done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers';
 import {Cta, Brand, Navbar} from './components'
+import './App.css'
 
 function App() {
   return (
     <div className='App' >
-      <div className='gradient_bg'>
+      <div className='gradient__bg'>
         <Navbar/>
         <Header/>
       </div>
